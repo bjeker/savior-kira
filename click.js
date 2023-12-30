e07aaa961e6a15909eff1ae38c122de1fdb49c3d
bjeker <rbieke1@gmail.com>
2023-12-29 21:30:06 -0600

    added js click file, moved buttons to js

---
+++ click.js
@@ -0,0 +1,23 @@
+function clicked(classClicked)
+{
+    if (classClicked == 'index')
+    {
+        window.location.href = 'index.html'
+    }
+    else if (classClicked == 'entrance')
+    {
+        window.location.href = 'entrance.html'
+    }
+    else if (classClicked == 'video')
+    {
+        window.location.href = 'video.html'
+    }
+    else if (classClicked == 'art')
+    {
+        window.location.href = 'art.html'
+    }
+    else if (classClicked == 'notebook')
+    {
+        window.location.href = 'notebook.html'
+    }
+}
